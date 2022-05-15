497bd46266ce7990c00af542c186c7435b49c232
Nicola <74636982+Nicolaadev@users.noreply.github.com>
2022-05-15 22:19:53 +0800

    Update ping.js

--- SlashCommands/info/ping.js
+++ SlashCommands/info/ping.js
@@ -24,3 +24,59 @@ module.exports = {
  * Please mention him / Rocket Community | Coding Lounge, when using this Code!
  * @INFO
  *********************************************************/
+
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "ping",
+    description: "returns websocket ping",
+    type: 'CHAT_INPUT',
+    run: async (client, interaction, args) => {
+        const Pinging = new MessageEmbed()
+        .setTitle("PINGING...")
+        .addField("Websocket Ping", "Searching", true)
+        .addField("API Ping", "Searching", true)
+        .addField("Message Edit Ping", "Searching", true)
+      const embed = await interaction.followUp({ embeds: [Pinging] });
+  
+      const websocket = interaction.createdTimestamp - Date.now();
+      const api = client.ws.ping.toLocaleString();
+      const msgedit = Math.floor(
+        embed.createdAt - interaction.createdAt
+      ).toLocaleString();
+  
+      let circles = {
+        green: "🟢",
+        yellow: "🟡",
+        red: "🔴",
+      };
+  
+      const websocketPing =
+        websocket <= 200
+          ? circles.green
+          : websocket <= 400
+          ? circles.yellow
+          : circles.red;
+      const apiPing =
+        api <= 200
+          ? circles.green
+          : api <= 400
+          ? circles.yellow
+          : circles.red;
+      const edit =
+        msgedit <= 200
+          ? circles.green
+          : msgedit <= 400
+          ? circles.yellow
+          : circles.red;
+  
+      const Pong = new MessageEmbed()
+        .setTitle("Pong 🏓")
+        .addField("Websocket Ping", `> ${websocketPing}${websocket.toLocaleString()}`, true)
+        .addField("API Ping", `> ${apiPing}${api}`, true)
+        .addField("Message Edit Ping", `> ${edit}${msgedit}`, true)
+        .setColor(client.colors.invis);
+      embed.edit({ embeds: [Pong] });
+      interaction.delete();
+    },
+}
