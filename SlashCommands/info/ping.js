b6dccb0ba1177df6517dfb1b95d24d88a9c77be7
Nicola <74636982+Nicolaadev@users.noreply.github.com>
2022-05-15 22:20:12 +0800

    Update ping.js

--- SlashCommands/info/ping.js
+++ SlashCommands/info/ping.js
@@ -1,30 +1,3 @@
-const { Client, CommandInteraction } = require("discord.js");
-
-module.exports = {
-    name: "ping",
-    description: "returns websocket ping",
-    type: 'CHAT_INPUT',
-    /**
-     *
-     * @param {Client} client
-     * @param {CommandInteraction} interaction
-     * @param {String[]} args
-     */
-    run: async (client, interaction, args) => {
-        interaction.followUp({ content: `${client.ws.ping}ms!` });
-    },
-};
-
-/**********************************************************
- * @INFO
- * Bot Coded by RyanChan#0204 | https://discord.gg/XBnRvZaHcc
- * @INFO
- * Code for Rocket Community | Coding Lounge | https://rocketdev.host
- * @INFO
- * Please mention him / Rocket Community | Coding Lounge, when using this Code!
- * @INFO
- *********************************************************/
-
 const { MessageEmbed } = require("discord.js")
 
 module.exports = {
@@ -80,3 +53,13 @@ module.exports = {
       interaction.delete();
     },
 }
+
+/**********************************************************
+ * @INFO
+ * Bot Coded by RyanChan#0204 | https://discord.gg/XBnRvZaHcc
+ * @INFO
+ * Code for Rocket Community | Coding Lounge | https://rocketdev.host
+ * @INFO
+ * Please mention him / Rocket Community | Coding Lounge, when using this Code!
+ * @INFO
+ *********************************************************/
